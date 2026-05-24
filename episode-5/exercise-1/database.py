1c8a13d98d99a76217124efe324846b7231fb0b0
DrRman25 <104178011+DrRman25@users.noreply.github.com>
2026-05-23 19:42:21 -0700

    Delete episode-5/exercise-1 directory

--- episode-5/exercise-1/database.py
+++
@@ -1,6 +0,0 @@
-# Dummy database file simulating inventory management
-inventory = {
-    "prod_01": {"name": "Coding Hoodie", "price": 45.00, "stock": 10},
-    "prod_02": {"name": "Mechanical Keyboard", "price": 120.00, "stock": 3},
-    "prod_03": {"name": "Python Sticker Pack", "price": 4.99, "stock": 100}
-}
--- episode-5/exercise-1/main.py
+++
@@ -1,6 +0,0 @@
-import processor
-
-# Main script simulating a user checkout session
-print("--- Welome to the Terminal Shop ---")
-order_total = processor.process("prod_01", 2)
-print(f"Your final total with tax is: ${order_total}")
